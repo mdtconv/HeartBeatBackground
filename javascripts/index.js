e109898995e13910d980a8ffb9505f70038cd209
Yoohogyun <yoohoogun114@naver.com>
2018-03-02 08:55:23 +0900

    Added Some Videos
    Former-commit-id: a813462fd979d748045e342c998b9be4e09f1773

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,10 +1,14 @@
 let video = document.getElementById('bm');
-var socket = io('http://13.125.124.80:3000');
-video.style.display = 'none';
-// video.pause();
-// video.play();
+let source = document.getElementById('source');
 
-let nowVisible = false;
+var socket = io('http://13.125.124.80:3000');
+// video.style.display = 'none';
+// video.pause();
+video.play();
+
+let enabled = false;
+let disabledVideos = ["./../videos/disabled_1.mp4"];
+let enabledVideos = ["./../videos/enabled_1.mp4", "./../videos/enabled_2.mp4", "./../videos/enabled_3.mp4", "./../videos/enabled_4.mp4", "./../videos/enabled_5.mp4"];
 
 socket.on('connect', () => {
     console.log('Connected');
@@ -12,23 +16,28 @@ socket.on('connect', () => {
 
 socket.on('heartbeat', (_heartbeat) => {
     console.log('got ', _heartbeat);
-    console.log('nowVisible is', nowVisible);
+    console.log('enabled is', enabled);
     if(_heartbeat > 0) { // valid
-        if(!nowVisible) {
+        if(!enabled) {
+            // enabled 변경
+            video.setAttribute('src',  enabledVideos[Math.floor(Math.random() * enabledVideos.length)]);
             video.load();
             video.play();
             fadeIn(video);
         }
     } else { // invalid
-        if(nowVisible) {
-            fadeOut(video);
+        if(enabled) {
+            enabled = false;
+            fadeIn(video, false);
+            video.setAttribute('src',  disabledVideos[Math.floor(Math.random() * disabledVideos.length)]);
+            video.play();
         }
     }
 });
 
-function fadeIn(element) {
+function fadeIn(element, enable=true) {
     var op = 0;
-    if(nowVisible === false) {
+    if(enabled === false) {
         element.style.display = 'inline';
         var timer = setInterval(function() {
             if (op >= 1) {
@@ -37,7 +46,7 @@ function fadeIn(element) {
             element.style.opacity = op;
             element.style.filter = 'alpha(opacity=' + op * 100 + ")";
             op += op * 0.1 || 0.1;
-            nowVisible = true;
+            enabled = enable;
         }, 50);
     }
 }
@@ -52,6 +61,6 @@ function fadeOut(element) {
         element.style.opacity = op;
         element.style.filter = 'alpha(opacity=' + op * 100 + ")";
         op -= op * 0.1;
-        nowVisible = false;
+        enabled = false;
     }, 50);
 }
